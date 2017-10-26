23ab89be5e2cbf1bdd82a38fe17c2c0bf6cd7f53
Alexander O'Hare <alexanderohare@Alexanders-MacBook-Pro.local>
2017-10-26 17:13:09 -0400

    Added basic file structure and sample site

---
+++ public_html/header.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Relational Retail</title>
+	<link rel="stylesheet" href="css/styles.css">
+</head>
+<body>
+	<div id="top-banner">
+		<header>Welcome to Relational Retail!</header>
+		<form id="login" action="login.php">
+		  	<label for="username">Username:</label>
+		  	<input type="text" name="username">
+		  	<label for="password">Password:</label>
+		  	<input type="password" name="password">
+		  	<input type="submit">
+		  </form>
+	</div>
+	<hr>
---
+++ public_html/index.php
@@ -0,0 +1,72 @@
+<?php include 'header.php' ?>
+<div id="container">
+    <div id="content">
+        <!-- Product Template -->
+        <div class="product-card">
+        	<!-- Product Image -->
+        	<img class="card-image" src="img/ps4_pro.jpg" alt="">
+        	<!-- Product Name -->
+        	<h5>Playstation 4 Pro</h5>
+        	<!-- Price -->
+        	<p>$298.99</p>
+					<button>Add to Cart</button>
+        </div> 
+        <!-- 									 -->
+
+        <div class="product-card">
+        	<!-- Product Image -->
+        	<img class="card-image" src="img/ark_ps4.jpg" alt="">
+        	<!-- Product Name -->
+        	<h5>Ark: Survival Evolved</h5>
+        	<!-- Price -->
+        	<p>$49.99</p>
+					<button>Add to Cart</button>
+        </div>
+        <div class="product-card">
+        	<!-- Product Image -->
+        	<img class="card-image" src="img/bioshock_ps4.jpg" alt="">
+        	<!-- Product Name -->
+        	<h5>Bioshock Collection</h5>
+        	<!-- Price -->
+        	<p>$39.99</p>
+					<button>Add to Cart</button>
+        </div>
+        <div class="product-card">
+        	<!-- Product Image -->
+        	<img class="card-image" src="img/darksiders_ps4.jpg" alt="">
+        	<!-- Product Name -->
+        	<h5>Darksiders</h5>
+        	<!-- Price -->
+        	<p>$39.99</p>
+					<button>Add to Cart</button>
+        </div>
+        <div class="product-card">
+        	<!-- Product Image -->
+        	<img class="card-image" src="img/destiny_2_ps4.jpg" alt="">
+        	<!-- Product Name -->
+        	<h5>Destiny 2</h5>
+        	<!-- Price -->
+        	<p>$59.99</p>
+					<button>Add to Cart</button>
+        </div>
+        <div class="product-card">
+        	<!-- Product Image -->
+        	<img class="card-image" src="img/dishonored_2_ps4.jpg" alt="">
+        	<!-- Product Name -->
+        	<h5>Dishonored 2</h5>
+        	<!-- Price -->
+        	<p>$29.99</p>
+					<button>Add to Cart</button>
+        </div>
+        <div class="product-card">
+        	<!-- Product Image -->
+        	<img class="card-image" src="img/doom_xbone.jpg" alt="">
+        	<!-- Product Name -->
+        	<h5>Doom</h5>
+        	<!-- Price -->
+        	<p>$39.99</p>
+					<button>Add to Cart</button>
+        </div>
+    </div>
+</div>
+<?php include 'footer.php' ?>
