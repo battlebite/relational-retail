f3ff89050976f4124bda328ff697ded0700be9d6
Logan Wenzel <lwenzel90@gmail.com>
2017-11-29 17:37:57 -0500

    Relational Retail with registering

---
+++ public_html/css/connect.php
@@ -0,0 +1,15 @@
+<?php
+	$dbuser = 'root';
+	$dbpassword = '';
+	$db = 'relational-retail';
+	$dbhost = 'localhost';
+	$dbport = 3306;
+	$mysqli = new mysqli($dbhost,$dbuser,$dbpassword,$db);
+	if (mysqli_connect_errno()) {
+		printf("Connect failed: %s\n", mysqli_connect_error());
+		exit();
+	}
+	function closeConnection() {
+		// mysqli_close($mysqli);
+	}
+	?>
